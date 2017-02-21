67508d097fd2dbfb4e9e03e0343dba0c6fdf6b0a
Ivan Pugach <ivanpugachit@mail.ru>
2017-02-21 20:02:09 +0200

    controller changes

--- src/main/java/app/controller/CatController.java
+++ src/main/java/app/controller/CatController.java
@@ -3,9 +3,11 @@ package app.controller;
 /**
  * Created by Иван on 20.02.2017.
  */
+
 import app.model.Cat;
 import app.repository.CatRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -15,30 +17,27 @@ import java.util.Map;
 
 
 @RestController
-@RequestMapping("/сat")
+
 public class CatController {
 
     @Autowired
     private CatRepository сatRepository;
 
-    @RequestMapping(method = RequestMethod.POST)
-    public Map<String, Object> createCat(@RequestBody Map<String, Object> сatMap) {
-        Cat сat = new Cat(сatMap.get("name").toString(),
-                сatMap.get("sex").toString(),
-                сatMap.get("birthday").toString());
-
+    @RequestMapping(method = RequestMethod.POST, value = "/cat/")
+    public Map<String, Object> createCat(@RequestBody Cat cat) {
         Map<String, Object> response = new LinkedHashMap<String, Object>();
         response.put("message", "сat created successfully");
-        response.put("сat", сatRepository.save(сat));
+        response.put("сat", сatRepository.save(cat));
         return response;
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/{сatId}")
-    public Cat getCatDetails(@PathVariable("сatId") String сatId) {
-        return сatRepository.findOne(сatId);
+    @RequestMapping(method = RequestMethod.GET, value = "/cat/{сatId}")
+    public ResponseEntity<Cat> getCatDetails(@PathVariable("сatId") String сatId) {
+        Cat cat = сatRepository.findOne(сatId);
+        return cat == null ? ResponseEntity.notFound().<Cat>build() : ResponseEntity.ok(cat);
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "/{сatId}")
+    @RequestMapping( method = RequestMethod.PUT,value = "/cat/{catID}")
     public Map<String, Object> editCat(@PathVariable("сatId") String сatId,
                                        @RequestBody Map<String, Object> сatMap) {
         Cat сat = new Cat(сatMap.get("name").toString(),
@@ -53,7 +52,7 @@ public class CatController {
     }
 
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "/{сatId}")
+    @RequestMapping(method = RequestMethod.DELETE, value = "/cat/{catId}" )
     public Map<String, String> deleteCat(@PathVariable("сatId") String сatId) {
         сatRepository.delete(сatId);
         Map<String, String> response = new HashMap<String, String>();
